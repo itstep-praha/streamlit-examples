33852c9b3e1023b1fe324cbe67b47431240d6293
Suche Ganbaatar <113457666+suche-cz@users.noreply.github.com>
2026-03-09 16:27:27 +0100

    Update page.py

--- page.py
+++ page.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 
 class Page:
+    """ Page helper/wrapper object to reduce code and structure pages """
+
     def __init__(self, path, title, icon, desc=''):
         self.path = path
         self.title = title
@@ -9,17 +11,19 @@ class Page:
         self.desc = desc
 
     def render(self):
-        print('RENDER', self.title)
+        """ renders a page header """
         st.title(self.icon + ' ' + self.title)
         st.write(self.desc)
         st.write('')
         st.write('')
 
     def render_container(self):
+        """ renders item as container for menu """
         with st.container(border=True):
             st.markdown(f'### {self.icon} {self.title}')
             if st.button(f'Otevřít {self.title}', key=self.path, use_container_width=True):
                 st.switch_page(self.path)
 
     def get_nav_item(self):
+        """ return st.Page object to create side nav item """
         return st.Page(self.path, title=self.title, icon=self.icon)
